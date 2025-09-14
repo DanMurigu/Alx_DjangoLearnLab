268285c0b3926f551cc4e07da71c07a5bc3db0ee
Daniel King'ara <murigukingara@gmail.com>
2025-09-14 20:34:21 +0300

    Permissions

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -14,6 +14,9 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
 
     def __str__(self):
         return self.title
@@ -41,11 +44,45 @@ class UserProfile(models.Model):
         ('Member', 'Member'),
     ]
 
-user = models.OneToOneField(User, on_delete=models.CASCADE)
-role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='Member')
 
-def __str__(self):
-    return f"{self.user.username} - {self.role}"
+
+
+    class Meta:
+        permissions = [
+            ("can_borrow_book", "Can borrow book"),
+            ("can_return_book", "Can return book"),
+        ]
+
+    def __str__(self):
+        return self.title
+
+
+class Library(models.Model):
+    name = models.CharField(max_length=200)
+    books = models.ManyToManyField(Book, related_name="libraries")
+
+    class Meta:
+        permissions = [
+            ("can_add_book_to_library", "Can add book to library"),
+            ("can_manage_library", "Can manage library"),
+        ]
+
+    def __str__(self):
+        return self.name
+
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name="librarian")
+
+    class Meta:
+        permissions = [
+            ("can_assign_librarian", "Can assign librarian"),
+            ("can_manage_librarian", "Can manage librarian"),
+        ]
+
+    def __str__(self):
+        return self.name
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
