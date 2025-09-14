178be3e0d0b2892eb10eaf249aa31d7d37c6c564
Daniel King'ara <murigukingara@gmail.com>
2025-09-14 19:43:21 +0300

    RBAC

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from .models import Book, Library
 from .models import Library
 from django.views.generic.detail import DetailView
-from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 
 
 # Create your views here.
@@ -48,4 +48,30 @@ def user_login(request):
 
 def user_logout(request):
     logout(request)
-    return render(request, 'relationship_app/logout.html')  
+    return render(request, 'relationship_app/logout.html')
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'   
+
+@login_required
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html') 
+
+@login_required
+@user_passes_test(is_admin) 
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@login_required
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
+
+
