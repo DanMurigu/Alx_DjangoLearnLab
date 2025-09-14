934d7af13276a8c9597d9f3644c51994cbe9e1ef
Daniel King'ara <murigukingara@gmail.com>
2025-09-14 20:24:35 +0300

    Permissions

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -51,7 +51,7 @@ def __str__(self):
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
-        
+
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.userprofile.save()
