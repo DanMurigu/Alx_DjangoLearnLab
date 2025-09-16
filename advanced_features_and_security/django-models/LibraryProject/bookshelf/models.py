7ce4b947da40200cece503ed7b3b4fbf72480dc0
Daniel King'ara <murigukingara@gmail.com>
2025-09-16 22:17:54 +0300

    Custom User

--- advanced_features_and_security/django-models/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/django-models/LibraryProject/bookshelf/models.py
@@ -9,5 +9,35 @@ class Book(models.Model):
     def __str__(self):
         return f"{self.title} by {self.author}"
 
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    
+class CustomUserManager(BaseUserManager):
+    
+    def create_user(self, username, email, password=None):
+        if not email:
+            raise ValueError("Users must have an email address")
 
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email)
+        user.set_password(password)  # hashes the password
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(username, email, password)
+
+objects = CustomUserManager()
+
+def __str__(self):
+    return self.username
     
