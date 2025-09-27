c74147fc8901a11e6c9fc66a4361135979018f24
Daniel King'ara <murigukingara@gmail.com>
2025-09-27 16:57:16 +0300

    CRUP ops

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,9 +3,16 @@ from django.views import View
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import generics
+from rest_framework import viewsets
+
 
 # Create your views here.
 class BookList(generics.ListAPIView):
     serializer_class = BookSerializer
     queryset = Book.objects.all()
+
+class BookViewSet(viewsets.ModelViewSet):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
+
 
