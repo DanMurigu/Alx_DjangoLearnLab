11bdca580404ac5a413b09b5e23ddd4f4b8ff673
Daniel King'ara <murigukingara@gmail.com>
2025-10-19 15:38:56 +0300

    Folder setup fixed

---
+++ social_media_api/accounts/serializers.py
@@ -0,0 +1,65 @@
+# accounts/serializers.py
+
+from rest_framework import serializers
+from django.contrib.auth import get_user_model, authenticate
+
+User = get_user_model()
+
+
+# -------------------------------
+# Serializer for User Registration
+# -------------------------------
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            password=validated_data['password'],
+            bio=validated_data.get('bio', '')
+        )
+        return user
+
+
+# -------------------------------
+# Serializer for Login
+# -------------------------------
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True, write_only=True)
+
+    def validate(self, data):
+        user = authenticate(username=data['username'], password=data['password'])
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Invalid credentials")
+
+
+# -------------------------------
+# Serializer for Profile 
+# -------------------------------
+class ProfileSerializer(serializers.ModelSerializer):
+    followers_count = serializers.SerializerMethodField()
+    following_count = serializers.SerializerMethodField()
+    profile_picture = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers_count', 'following_count']
+
+    def get_followers_count(self, obj):
+        return obj.followers.count()
+
+    def get_following_count(self, obj):
+        return obj.following.count()
+
+    def get_profile_picture(self, obj):
+        request = self.context.get('request')
+        if obj.profile_picture and request:
+            return request.build_absolute_uri(obj.profile_picture.url)
+        return None
