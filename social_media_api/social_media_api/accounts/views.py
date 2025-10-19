d522bb2ccf24ed6b033a292f9646abd5cc67028c
Daniel King'ara <murigukingara@gmail.com>
2025-10-19 15:34:03 +0300

    api view fixed

--- social_media_api/social_media_api/accounts/views.py
+++ social_media_api/social_media_api/accounts/views.py
@@ -1,3 +1,64 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model
+from .serializers import RegisterSerializer, LoginSerializer
+from rest_framework.views import APIView
 
 # Create your views here.
+User = get_user_model()
+
+
+# -------------------------------
+# Register New Users
+# -------------------------------
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({
+            "user": RegisterSerializer(user).data,
+            "token": token.key
+        })
+
+
+# -------------------------------
+# Login Users
+# -------------------------------
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({
+            "user": RegisterSerializer(user).data,
+            "token": token.key
+        })
+
+
+# -------------------------------
+# Get Authenticated User Profile
+# -------------------------------
+class ProfileView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        return Response({
+            "username": user.username,
+            "email": user.email,
+            "bio": user.bio,
+            "profile_picture": request.build_absolute_uri(user.profile_picture.url) if user.profile_picture else None,
+            "followers_count": user.followers.count(),
+            "following_count": user.following.count(),
+        })
