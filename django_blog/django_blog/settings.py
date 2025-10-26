4a07bd3011475c3d7e042af40ad063cf186d34a8
Daniel King'ara <murigukingara@gmail.com>
2025-10-26 22:43:11 +0300

    Database configuration

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -75,13 +75,16 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
+DDATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_blog_db',
+        'USER': 'root',         
+        'PASSWORD': '@Dan2403#',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
