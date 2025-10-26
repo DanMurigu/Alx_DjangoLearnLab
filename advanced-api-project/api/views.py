c8eaf7b02e321c58a2b13d3c5f1e820273ca6d30
Daniel King'ara <murigukingara@gmail.com>
2025-10-26 19:42:52 +0300

    Search and filtering

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,9 +3,9 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Author, Book
 from .serializers import AuthorSerializer, BookSerializer
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-
+from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
 # Returns a list of all authors with their nested books
@@ -32,6 +32,12 @@ class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.AllowAny] # Anyone can view books
+
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['title', 'author__name', 'publication_year']
+    search_fields = ['title', 'author__name']
+    ordering_fields = ['title', 'publication_year']
+    ordering = ['title']
 
 
 # DetailView – Retrieve a single book by ID
