c4d39e6e96eb7d5202d63faf8354cab73f7d789c
Daniel King'ara <murigukingara@gmail.com>
2025-10-26 19:24:03 +0300

    Url fix

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -11,9 +11,8 @@ from .views import (
 urlpatterns = [
     path('authors/', views.author_list, name='author_list'),
     path('books/', views.book_list, name='book_list'),
-    path('books/list/', BookListView.as_view(), name='book_list_view'),
-    path('books/<int:pk>/', BookDetailView.as_view(), name='book_detail_view'),
-    path('books/create/', BookCreateView.as_view(), name='book_create_view'),
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book_update_view'),
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book_delete_view'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+    path('books/create/', BookCreateView.as_view(), name='book-create'),
+    path('books/update/<int:pk>/', BookUpdateView.as_view(), name='book-update'),
+    path('books/delete/<int:pk>/', BookDeleteView.as_view(), name='book-delete'),
 ]
