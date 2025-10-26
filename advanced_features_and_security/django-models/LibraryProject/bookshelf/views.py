36f52fc1e40ab171aeb67b1e6c68065a8f516660
Daniel King'ara <murigukingara@gmail.com>
2025-10-26 11:45:29 +0300

    Views fix

--- advanced_features_and_security/django-models/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/django-models/LibraryProject/bookshelf/views.py
@@ -3,9 +3,14 @@ from django.contrib.auth.decorators import permission_required
 from .models import Book
 
 @permission_required('bookshelf.can_view', raise_exception=True)
+
 def book_list(request):
-    books = Book.objects.all()
-    return render(request, 'bookshelf/book_list.html', {'books': books, 'book_list': books})
+    books = Book.objects.all()  # Fetch all books
+    book_list = books  # Explicit variable so both names exist
+    return render(request, 'bookshelf/book_list.html', {
+        'books': books,
+        'book_list': book_list
+    })
 
 @permission_required('bookshelf.can_create', raise_exception=True)
 def create_book(request):
@@ -21,3 +26,4 @@ def edit_book(request, book_id):
 def delete_book(request, book_id):
     # handle delete logic
     pass
+
