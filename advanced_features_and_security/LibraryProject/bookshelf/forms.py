0fd8ec303cad37dd359743825bb6b64275800c5c
Daniel King'ara <murigukingara@gmail.com>
2025-10-26 12:29:23 +0300

    Example form

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,1 +1,5 @@
 from django import forms
+
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100, label="Example Name")
+    description = forms.CharField(widget=forms.Textarea, label="Description")
