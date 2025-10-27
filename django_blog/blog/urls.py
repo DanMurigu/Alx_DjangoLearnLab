ca5523fbd2e41fc22555169ba4a6e3f75519a790
Daniel King'ara <murigukingara@gmail.com>
2025-10-27 06:56:05 +0300

    Spelling fix

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -15,9 +15,9 @@ urlpatterns = [
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),# Delete
 
     # Comment URLs
-    path('posts/<int:post_pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
-    path('comments/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
-    path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
+    path('post/<int:post_pk>/comment/new/', CommentCreateView.as_view(), name='comment-create'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 
     # Tag and Search URLs
     path('search/', search_posts, name='search-posts'),
